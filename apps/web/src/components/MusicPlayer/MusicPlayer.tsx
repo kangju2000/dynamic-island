3dc9c5907dfe826e3156d10bee0b2f76b13af332
Juhyeok Kang <kangju2000@naver.com>
2024-05-12 04:19:24 +0900

    feat: 뒤로/앞으로 모션 추가

--- apps/web/src/components/MusicPlayer/MusicPlayer.tsx
+++ apps/web/src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,9 +1,9 @@
 import { css } from '@emotion/react';
 import { DynamicIsland } from '@kangju2000/dynamic-island';
 import { getSvgPath } from 'figma-squircle';
-import { AnimatePresence, cubicBezier, motion, useAnimate } from 'framer-motion';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
-import { IconBack, IconPause, IconNext, IconAirplay, IconPlay } from '../../assets/index';
+import { IconPause, IconAirplay, IconPlay } from '../../assets/index';
 import { Bar } from './Bar';
 import { Equalizer } from './Equalizer';
 import { musicList } from './const';
@@ -21,6 +21,9 @@ export function MusicPlayer() {
   const [scopeThumbnail, animateThumbnail] = useAnimate();
   const thumbnailPath = getSvgPath({ width: 60, height: 60, cornerRadius: 16, cornerSmoothing: 0.6 });
   const initialRender = useRef<boolean>(true);
+  const timeoutId = useRef<number>(0);
+  const [prevArrowList, setPrevArrowList] = useState([1, 2, 3]);
+  const [nextArrowList, setNextArrowList] = useState([1, 2, 3]);
 
   const currentMusic = musicList[currentMusicIndex];
 
@@ -75,6 +78,12 @@ export function MusicPlayer() {
     );
   }, [currentMusicIndex]);
 
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutId.current);
+    };
+  }, []);
+
   return (
     <DynamicIsland.Expanded>
       <motion.div
@@ -121,8 +130,42 @@ export function MusicPlayer() {
         <div style={{ height: '8px' }} />
 
         <div css={playerWrapperCss}>
-          <motion.div>
-            <IconBack width={38} height={38} onClick={handlePrevMusic} />
+          <motion.div
+            whileTap={{ scale: 0.9, backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
+            css={playerIconAreaCss}
+            onClick={() => {
+              handlePrevMusic();
+              timeoutId.current = setTimeout(() => {
+                setPrevArrowList(prev => {
+                  const temp = [...prev];
+                  const first = temp.shift();
+                  temp.push(first!);
+                  return temp;
+                });
+              }, 150);
+            }}
+          >
+            <AnimatePresence mode="popLayout">
+              {prevArrowList.slice(0, 2).map(item => {
+                return (
+                  <motion.div
+                    key={item}
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    transition={{ duration: 0.3 }}
+                    layout
+                  >
+                    <svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M15.386 19C14.9188 19 14.5047 18.8408 13.9738 18.533L1.36977 11.2302C0.424734 10.6888 -6.80448e-08 10.2324 -6.80448e-08 9.5C-6.80448e-08 8.77821 0.424734 8.32179 1.36977 7.76983L13.9738 0.467039C14.5047 0.159218 14.9188 0 15.386 0C16.3098 0 17 0.711173 17 1.84693V17.1531C17 18.2994 16.3098 19 15.386 19Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
           </motion.div>
 
           <AnimatePresence mode="popLayout">
@@ -133,7 +176,7 @@ export function MusicPlayer() {
               exit={{ opacity: 0 }}
               transition={{ delay: 0.08 }}
               whileTap={{ scale: 0.9, backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
-              css={pauseCss}
+              css={playerIconAreaCss}
               onClick={() => setIsPlaying(prev => !prev)}
             >
               <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} exit={{ scale: 0 }}>
@@ -142,8 +185,42 @@ export function MusicPlayer() {
             </motion.div>
           </AnimatePresence>
 
-          <motion.div>
-            <IconNext width={38} height={38} onClick={handleNextMusic} />
+          <motion.div
+            whileTap={{ scale: 0.9, backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
+            css={playerIconAreaCss}
+            onClick={() => {
+              handleNextMusic();
+              timeoutId.current = setTimeout(() => {
+                setNextArrowList(prev => {
+                  const temp = [...prev];
+                  const last = temp.pop();
+                  temp.unshift(last!);
+                  return temp;
+                });
+              }, 150);
+            }}
+          >
+            <AnimatePresence mode="popLayout">
+              {nextArrowList.slice(-2).map(item => {
+                return (
+                  <motion.div
+                    key={item}
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    transition={{ duration: 0.3 }}
+                    layout
+                  >
+                    <svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M1.61399 19C2.0812 19 2.49532 18.8408 3.02623 18.533L15.6302 11.2302C16.5753 10.6888 17 10.2324 17 9.5C17 8.77821 16.5753 8.32179 15.6302 7.76983L3.02623 0.467039C2.49532 0.159218 2.0812 0 1.61399 0C0.690194 0 0 0.711173 0 1.84693V17.1531C0 18.2994 0.690194 19 1.61399 19Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
           </motion.div>
           <IconAirplay css={airplayCss} width={32} height={32} />
         </div>
@@ -217,20 +294,18 @@ const playerWrapperCss = css({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  gap: '16px',
   svg: {
     cursor: 'pointer',
   },
 });
 
-const pauseCss = css({
+const playerIconAreaCss = css({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   width: '60px',
   height: '60px',
   borderRadius: '50%',
-  // backgroundColor: 'rgba(255, 255, 255, 0.1)',
 });
 
 const airplayCss = css({
